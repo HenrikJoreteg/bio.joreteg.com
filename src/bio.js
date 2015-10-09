aee5bf1edfbb0c069ac0c1888b5645d564d859e1
Henrik Joreteg <hjoreteg@gmail.com>
2015-10-09 11:29:09 -0700

    tweaks

--- src/bio.js
+++ src/bio.js
@@ -42,11 +42,11 @@ export default class Bio extends Component {
         </p>
 
         <h2>Short Bio:</h2>
-        <p>Open Sourcer, blogger, author, and educator. Made [Ampersand.js](http://ampersandjs.com/), [Human JavaScript](http://read.humanjavascript.com/), [Talky.io](https://talky.io/), and [SimpleWebRTC](http://simplewebrtc.com/).</p>
+        <p>Open Sourcer, blogger, author, and educator. Made <a href='http://ampersandjs.com'>Ampersand.js</a>, <a href='http://read.humanjavascript.com'>Human JavaScript</a>, <a href='https://talky.io'>Talky.io</a>], and <a href='http://simplewebrtc.com'>SimpleWebRTC</a>.</p>
 
         <h2>Bio:</h2>
         <p>Henrik Joreteg is a JavaScript developer, author, speaker, and educator.</p>
-        <p>He has pioneered many techniques and tools for building JavaScript apps especially those with a realtime aspect. Most recently, he lead development on the <a href='http://ampersandjs.com'>Ampersand.js framework</a> that has seen rapid adoption by the likes of WhatsApp, FlipKart, Shutterfly and many others. He also created the <a href="http://simplewebrtc.com">SimpleWebRTC</a> library, <a href="https://talky.io/">Talky.io</a>, is the author of the book <a href="http://humanjavascript.com">“Human JavaScript”</a>, and frequently speaks at JS events.</p>
+        <p>He has pioneered many techniques and tools for building JavaScript apps especially those with a realtime aspect. Most recently, he lead development on the <a href='http://ampersandjs.com'>Ampersand.js framework</a> that has seen rapid adoption by the likes of WhatsApp, FlipKart, Shutterfly and many others. He also created the <a href='http://simplewebrtc.com'>SimpleWebRTC</a> library, <a href='https://talky.io/'>Talky.io</a>, is the author of the book <a href='http://humanjavascript.com'>“Human JavaScript”</a>, and frequently speaks at JS events.</p>
         <p>Having released <a href='https://www.npmjs.com/~henrikjoreteg'>over 200 JavaScript libraries</a>, Henrik is a huge proponent of open source and wants to help developers push the web to the next level.</p>
         <p>You can follow him on Twitter at <a href='http://twitter.com/henrikjoreteg'>@HenrikJoreteg</a> or keep an eye out for his posts at <a href='http://joreteg.com'>joreteg.com</a>.</p>
 
