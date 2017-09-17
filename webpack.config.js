dbd29f92a23c6fd468c814fbbf4be19b483f7d77
Henrik Joreteg <hjoreteg@gmail.com>
2017-09-16 21:31:10 -0700

    updating deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
-require('babel/register')
-var React = require('react')
-var getConfig = require('hjs-webpack')
-var Bio = require('./src/bio')
+require('babel-register')
+const React = require('react')
+const { renderToStaticMarkup } = require('react-dom/server')
+const getConfig = require('hjs-webpack')
+const Bio = require('./src/bio').default
 
 module.exports = getConfig({
   in: 'src/app.js',
   out: 'public',
   clearBeforeBuild: true,
   html: function (context) {
-    var html = React.renderToStaticMarkup(React.createElement(Bio))
+    var html = renderToStaticMarkup(React.createElement(Bio))
     return '<!doctype html><head><meta charset="utf-8"/><meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/><link rel="stylesheet" href="' + context.css + '"/></head><body>' + html + '</body>'
   }
 })
